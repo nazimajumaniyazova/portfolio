cb890c5e82c034c1a6f896bf0b35771354b32129
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2022-11-04 19:09:49 +0500

    feat: remove coding style section

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,4 +1,5 @@
-const mainItems = document.querySelectorAll(".main section")
+// const mainItems = document.querySelectorAll(".main section");
+
 ScrollReveal({
     reset: true,
     interval: 200,
@@ -6,6 +7,11 @@ ScrollReveal({
     origin: 'bottom',
     distance: '100px'
 });
-mainItems.forEach((section) => {
-        ScrollReveal().reveal(`.${section.getAttribute("class")}`)
-})
+
+// mainItems.forEach((section) => {
+//         ScrollReveal().reveal(`.${section.getAttribute("class")}`);
+// })
+
+const introSection = document.querySelector('.about');
+//ScrollReveal().reveal('.about', {delay: 1500, distance: '200px', duration: 600, interval: 500,  origin: 'top'});
+
