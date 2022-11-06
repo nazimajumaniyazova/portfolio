0f5499e04c1c6cf7946ec11dbd56b2e11db41655
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2022-11-06 19:35:52 +0500

    feat: add header funcionality

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,12 +1,34 @@
 const mainContent = document.querySelector('.main')
 const menuButton = document.querySelector('.menu-button')
 const header = document.querySelector('.header')
+
+const burgerBtn = document.querySelector('.burger')
+
+const nav = document.querySelector(".nav")
 menuButton.addEventListener('click', (e)=>{
     const eventTarget = e.target.closest('.menu-button')
+    if(menuButton.querySelector('.burger-close')){
+      burgerBtn.classList.remove('burger-close')
+      header.style.transform = `translate(100%, 0)`
+      mainContent.style.transform = `translate(0, 0)`
+      mainContent.style.opacity = `1`
+      return
+    }
     header.style.transition = `all .3s ease`
     header.style.transform = `translate(0, 0)`
     mainContent.style.transition = `all .3s ease`
     mainContent.style.transform = `translate(-24%, 0)`
     mainContent.style.opacity = `0.45`
-    console.log('lss')
+    burgerBtn.classList.add("burger-close")
+})
+
+window.addEventListener('click', (event) => {
+  console.log(!!event.target.closest('.main'))
+  if (!!event.target.closest('.main') && menuButton.querySelector('.burger-close')) {
+    console.log('l')
+    burgerBtn.classList.remove('burger-close')
+    header.style.transform = `translate(100%, 0)`
+    mainContent.style.transform = `translate(0, 0)`
+    mainContent.style.opacity = `1`
+  }
 })
