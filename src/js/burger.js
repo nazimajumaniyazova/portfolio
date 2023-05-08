eb9cc93af7d0a127e786608ae3fbe1d4a411753b
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-05-08 17:33:10 +0500

    feat: fix minor issues

--- src/js/burger.js
+++ src/js/burger.js
@@ -10,15 +10,7 @@ const nav = document.querySelector('.nav');
 
 menuButton.addEventListener('click', (e) => {
   if (menuButton.querySelector('.burger-close')) {
-    // if (sizeWidth <= 800) {
-    //   rightContent.style.left = '50%';
-    // } else {
-    //   rightContent.style.left = '25%';
-    // }
     burgerBtn.classList.remove('burger-close');
-    // header.style.transform = `translate(100%, 0)`;
-    // mainContent.style.left = `0%`;
-    // mainContent.style.opacity = `1`;
     header.classList.remove('header_active');
     mainContent.classList.remove('main_moved');
     rightContent.classList.remove('right__content_moved');
@@ -28,12 +20,6 @@ menuButton.addEventListener('click', (e) => {
   mainContent.classList.add('main_moved');
   header.classList.add('header_active');
   rightContent.classList.add('right__content_moved');
-  // header.style.transition = `all .5s ease`;
-  // header.style.transform = `translate(0, 0)`;
-  // mainContent.style.transition = `all .3s ease`;
-  // mainContent.style.left = `-24%`;
-  // mainContent.style.opacity = `0.45`;
-  // rightContent.style.left = '3%';
   burgerBtn.classList.add('burger-close');
 });
 
@@ -42,11 +28,6 @@ window.addEventListener('click', (event) => {
     !!event.target.closest('.main') &&
     menuButton.querySelector('.burger-close')
   ) {
-    // if (sizeWidth <= 800) {
-    //   rightContent.style.left = '50%';
-    // } else {
-    //   rightContent.style.left = '25%';
-    // }
     burgerBtn.classList.remove('burger-close');
     header.classList.remove('header_active');
     mainContent.classList.remove('main_moved');
@@ -56,11 +37,6 @@ window.addEventListener('click', (event) => {
 menu.addEventListener('click', (event) => {
   const eventTarget = event.target.closest('.menu__item');
   if (eventTarget) {
-    // if (sizeWidth <= 800) {
-    //   rightContent.style.left = '50%';
-    // } else {
-    //   rightContent.style.left = '25%';
-    // }
     header.classList.remove('header_active');
     burgerBtn.classList.remove('burger-close');
     mainContent.classList.remove('main_moved');
--- src/js/download-cv.js
+++ src/js/download-cv.js
@@ -1,15 +1,16 @@
-const cvLink = document.querySelector('.download-cv')
+const cvLink = document.querySelector('.download-cv');
 
-async function downloadCV(){
-  const cvFile = await fetch('http://127.0.0.1:5500/src/files/CV_Nazima_Jumaniyazova_EN.pdf')
+async function downloadCV() {
+  const cvFile = await fetch(
+    'https://nazimajumaniyazova.github.io/portfolio/src/files/CV_Nazima_Jumaniyazova_EN.pdf'
+  );
   const cvFileBlob = await cvFile.blob();
 
   const aElement = document.createElement('a');
   aElement.setAttribute('download', 'CV_Nazima_Jumaniyazova_EN.pdf');
   const href = URL.createObjectURL(cvFileBlob);
   aElement.href = href;
-  //aElement.setAttribute('target', '_blank');
   aElement.click();
   URL.revokeObjectURL(href);
 }
-cvLink.addEventListener('click', downloadCV)
+cvLink.addEventListener('click', downloadCV);
