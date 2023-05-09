05d3c9c90d87f81ce0c027fbbbfdedd97063b282
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-05-09 10:57:45 +0500

    feat: add animated bg

---
+++ src/js/bg.js
@@ -0,0 +1,12 @@
+VANTA.GLOBE({
+  el: '#bg',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 100.0,
+  minWidth: 100.0,
+  scale: 0.5,
+  scaleMobile: 1.0,
+  color: '#54595f',
+  backgroundColor: '#222122',
+});
